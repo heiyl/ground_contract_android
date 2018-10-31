9ea407093ce0beffcd6f250b74a443cb2c7584ac
heiyulong <heiyulong@xdf.cn>
2018-10-31 10:48:36 +0800

    主页面内容信息

--- app/src/main/java/com/diyuewang/m/MainActivity.java
+++ app/src/main/java/com/diyuewang/m/MainActivity.java
@@ -1,14 +1,38 @@
 package com.diyuewang.m;
 
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatEditText;
 import android.view.KeyEvent;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.diyuewang.m.base.BaseToolBarActivity;
 import com.diyuewang.m.tools.UIUtils;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends BaseToolBarActivity {
+
+    //流转区域视图
+    @BindView(R.id.rl_location)
+    RelativeLayout rl_location;
+
+    //流转区域内容
+    @BindView(R.id.tv_location_content)
+    TextView tv_location_content;
+
+    //流转方式视图
+    @BindView(R.id.rl_select_type)
+    RelativeLayout rl_select_type;
+
+    //流转方式内容
+    @BindView(R.id.tv_select_type_content)
+    TextView tv_select_type_content;
+
+    //流转面积
+    @BindView(R.id.edt_size_content)
+    AppCompatEditText edt_size_content;
 
     private long firstTime = 1;
 
