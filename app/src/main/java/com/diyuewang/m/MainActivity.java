72401e56cc48b7830914a947d2344eed1b431682
heiyulong <heiyulong@xdf.cn>
2018-10-31 13:33:14 +0800

    滚动选择空间添加

--- app/src/main/java/com/diyuewang/m/MainActivity.java
+++ app/src/main/java/com/diyuewang/m/MainActivity.java
@@ -3,16 +3,19 @@ package com.diyuewang.m;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.diyuewang.m.base.BaseToolBarActivity;
+import com.diyuewang.m.dialog.SelectPopupWindow;
 import com.diyuewang.m.tools.UIUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends BaseToolBarActivity {
+public class MainActivity extends BaseToolBarActivity implements View.OnClickListener, SelectPopupWindow.WheelViewListener {
 
     //流转区域视图
     @BindView(R.id.rl_location)
@@ -34,7 +37,12 @@ public class MainActivity extends BaseToolBarActivity {
     @BindView(R.id.edt_size_content)
     AppCompatEditText edt_size_content;
 
+    @BindView(R.id.rlt_root)
+    RelativeLayout rlt_root;
+
     private long firstTime = 1;
+
+    private SelectPopupWindow menuWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +51,11 @@ public class MainActivity extends BaseToolBarActivity {
         ButterKnife.bind(this);
         initStatusBar(true);
         initToolBar(getString(R.string.title_main),false);
+        initView();
+    }
+
+    private void initView() {
+        rl_select_type.setOnClickListener(this);
     }
 
     @Override
@@ -65,5 +78,19 @@ public class MainActivity extends BaseToolBarActivity {
     public void onBackPressed() {
         back();
         overridePendingTransition(0, 0);
+    }
+
+    @Override
+    public void getContent(String content) {
+        tv_select_type_content.setText(content);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.rl_select_type:
+                menuWindow = new SelectPopupWindow(activity, rlt_root,this);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/diyuewang/m/dialog/SelectPopupWindow.java
@@ -0,0 +1,95 @@
+package com.diyuewang.m.dialog;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.contrarywind.listener.OnItemSelectedListener;
+import com.contrarywind.view.WheelView;
+import com.diyuewang.m.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class SelectPopupWindow extends PopupWindow {
+
+    private final List<String> mOptionsItems;
+    private TextView tv_cancle, tv_sure;
+    private WheelView wheelview;
+    WheelViewListener listener;
+    private String content;
+
+    public interface WheelViewListener{
+        void getContent(String content);
+    }
+
+    public SelectPopupWindow(final Context mContext, View parent, final WheelViewListener listener) {
+        super(mContext);
+        View view = View.inflate(mContext, R.layout.dialog_select_popupwindows, null);
+        view.invalidate();
+        view.startAnimation(AnimationUtils.loadAnimation(mContext, R.anim.fade_ins));
+        LinearLayout ll_popup = (LinearLayout) view.findViewById(R.id.ll_popup);
+        RelativeLayout root = (RelativeLayout) view.findViewById(R.id.root);
+        ll_popup.startAnimation(AnimationUtils.loadAnimation(mContext, R.anim.push_bottom_in));
+
+        setWidth(LayoutParams.FILL_PARENT);
+        setHeight(LayoutParams.FILL_PARENT);
+        setBackgroundDrawable(new BitmapDrawable());
+        setFocusable(true);
+        setOutsideTouchable(true);
+        setContentView(view);
+        showAtLocation(parent, Gravity.BOTTOM, 0, 0);
+        // update();
+
+        wheelview = (WheelView) view.findViewById(R.id.wheelview);
+        wheelview.setCyclic(false);
+
+        Resources res = mContext.getResources();
+        String[] city = res.getStringArray(R.array.select_type);
+        mOptionsItems = Arrays.asList(city);
+        content = mOptionsItems.get(0);
+        wheelview.setAdapter(new ArrayWheelAdapter(mOptionsItems));
+        wheelview.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int index) {
+                content = mOptionsItems.get(index);
+//                Toast.makeText(mContext, "" + mOptionsItems.get(index), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tv_cancle = (TextView) view.findViewById(R.id.tv_cancle);
+        tv_sure = (TextView) view.findViewById(R.id.tv_sure);
+        tv_cancle.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        tv_sure.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                if(listener != null){
+                    listener.getContent(content);
+                }
+                dismiss();
+            }
+        });
+        root.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+    }
+
+}
